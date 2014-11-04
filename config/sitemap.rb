17ccb37321ddc4f797497bd7eea94737fc992a24
philippe-nougaillon <philippe.nougaillon@gmail.com>
2014-11-04 15:18:06 +0100

    nouveau sitemap.xml

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,6 @@
 # Change this to your host. See the readme at https://github.com/lassebunk/dynamic_sitemaps
 # for examples of multiple hosts and folders.
-host "http://demonstration.opencantine.net"
+host "demonstration.opencantine.net"
 
 sitemap :site do
   url root_url, last_mod: Time.now, change_freq: "daily", priority: 1.0
